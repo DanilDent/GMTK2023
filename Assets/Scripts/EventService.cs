ecb49c41495878a0848f8aacd508e021b6868b03
Danil Dent <farctate11@gmail.com>
2023-07-08 14:29:12 +0400

    [GameTimeline] Make EventService Singleton

--- Assets/Scripts/EventService.cs
+++ Assets/Scripts/EventService.cs
@@ -2,7 +2,27 @@ using System;
 
 public class EventService
 {
+    // Singleton Impl
+    protected EventService() { }
+
+    private static EventService _instance;
+    public static EventService Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new EventService();
+            }
+
+            return _instance;
+        }
+    }
+
+    // Events
     public Action<QuestConfig> NewQuestBecomeAvailable;
+    // Passes current GameTime
+    public Action<GameTime> GameTimeUpdated;
 }
 
 
