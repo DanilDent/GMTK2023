3da7cc00e5cf0ffa9c107bc434d126022cf84b61
Vasily Fateev <vasily.fat@gmail.com>
2023-07-08 21:59:34 +0300

    Add City

---
+++ Assets/Scripts/CityManager/CityTests.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CityTests : MonoBehaviour
+{
+    [SerializeField] private City city;
+    private void Start()
+    {
+        city = FindAnyObjectByType<City>();
+        if (city == null)
+        {
+            return;
+        }
+
+        EventService.Instance.QuestCompleted?.Invoke(QuestManager.Instance.invisibleQuests[0], false);
+        Debug.Log(city.CurrentHealth);
+        EventService.Instance.QuestCompleted?.Invoke(QuestManager.Instance.invisibleQuests[0], true);
+        Debug.Log(city.CurrentHealth);
+        city.ApplyChange(20);
+        Debug.Log(city.CurrentHealth);
+        city.ApplyChange(-5);
+        Debug.Log(city.CurrentHealth);
+        city.ApplyChange(3);
+        Debug.Log(city.CurrentHealth);
+        city.ApplyChange(-50);
+        Debug.Log(city.CurrentHealth);
+    }
+}
