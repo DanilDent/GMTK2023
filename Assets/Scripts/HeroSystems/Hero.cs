427cc9f06fbedaa8f32ea203880125027290edca
f0kes <f0kesppokes@gmail.com>
2023-07-09 11:03:07 +0300

    change avatar on quest assigned

--- Assets/Scripts/HeroSystems/Hero.cs
+++ Assets/Scripts/HeroSystems/Hero.cs
@@ -6,52 +6,71 @@ using UnityEngine;
 [Serializable]
 public struct Hero
 {
-    public string HeroBehPatternName => _heroBehPatternName;
+	public string HeroBehPatternName => _heroBehPatternName;
 
-    [SerializeField] private string _heroBehPatternName;
+	[SerializeField] private string _heroBehPatternName;
 
-    [Serializable]
-    public struct Avatar
-    {
-        [SerializeField] public Sprite Value;
-    }
-    [Serializable]
-    public struct QuestMoodBonus
-    {
-        [SerializeField] public string QuestName;
-        [SerializeField] public int MoodBonus;
-    }
-    [Serializable]
-    public struct HeroMoodRange
-    {
-        [SerializeField] public HeroMood HeroMood;
-    }
-    [SerializeField] private string _nickname;
-    [SerializeField] private QuestMoodBonus[] _questMoodBonuses;
-    [SerializeField] private List<HeroMood> _heroMoods;
-    private Dictionary<string, int> _bonuses;
+	[Serializable]
+	public struct Avatar
+	{
+		[SerializeField] public Sprite Value;
+	}
+	[Serializable]
+	public struct QuestMoodBonus
+	{
+		[SerializeField] public string QuestName;
+		[SerializeField] public int MoodBonus;
+		[SerializeField] public Sprite AvatarOnQuestAssigned;
+	}
+	[Serializable]
+	public struct HeroMoodRange
+	{
+		[SerializeField] public HeroMood HeroMood;
+	}
+	[SerializeField] private string _nickname;
+	[SerializeField] private QuestMoodBonus[] _questMoodBonuses;
+	[SerializeField] private List<HeroMood> _heroMoods;
+	private Dictionary<string, int> _bonuses;
+	private Dictionary<string, Sprite> _avatarOnQuestAssigned;
+
+	public Dictionary<string, int> Bonuses
+	{
+		get
+		{
+			if(_bonuses != null) return _bonuses;
+			_bonuses = new Dictionary<string, int>();
+			foreach(var bonus in _questMoodBonuses)
+			{
+				_bonuses[bonus.QuestName] = bonus.MoodBonus;
+			}
+			return _bonuses;
+		}
+
+	}
+
+	public Dictionary<string, Sprite> AvatarOnQuestAssigned
+	{
+		get
+		{
+			if(_avatarOnQuestAssigned != null) return _avatarOnQuestAssigned;
+			_avatarOnQuestAssigned = new Dictionary<string, Sprite>();
+			foreach(var bonus in _questMoodBonuses)
+			{
+				if(bonus.AvatarOnQuestAssigned != null)
+				{
+					_avatarOnQuestAssigned[bonus.QuestName] = bonus.AvatarOnQuestAssigned;
+				}
+			}
+			return _avatarOnQuestAssigned;
+		}
+	}
 
 
-    public Dictionary<string, int> Bonuses
-    {
-        get
-        {
-            if (_bonuses != null) return _bonuses;
-            _bonuses = new Dictionary<string, int>();
-            foreach (var bonus in _questMoodBonuses)
-            {
-                _bonuses[bonus.QuestName] = bonus.MoodBonus;
-            }
-            return _bonuses;
-        }
+	public string Nickname => _nickname;
 
-    }
-
-    public string Nickname => _nickname;
-
-    public int CurrentMoodScore;
-    public HeroMood CurrentHeroMood;
-    public List<HeroMood> HeroMoods => _heroMoods;
-    public List<Avatar> CurrentAvatarParts;
+	public int CurrentMoodScore;
+	public HeroMood CurrentHeroMood;
+	public List<HeroMood> HeroMoods => _heroMoods;
+	public List<Avatar> CurrentAvatarParts;
 
 }
