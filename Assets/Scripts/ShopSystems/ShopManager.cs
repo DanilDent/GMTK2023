969d57caf52a71bf1cdfbe24523b4e901cc56ef4
Danil Dent <farctate11@gmail.com>
2023-07-09 15:56:59 +0400

    fix errors after diag manager merge

--- Assets/Scripts/ShopSystems/ShopManager.cs
+++ Assets/Scripts/ShopSystems/ShopManager.cs
@@ -1,41 +1,39 @@
-﻿using System;
-
-namespace GitIntegration.ShopSystems
+﻿namespace GitIntegration.ShopSystems
 {
-	public class ShopManager : MonoSingleton<ShopManager>
-	{
-		private void Start()
-		{
-			SubscribeToEvents();
-		}
-		
-		protected override void OnDestroy()
-		{
-			if(Instance == this)
-			{
-				UnsubscribeFromEvents();	
-			}
-			base.OnDestroy();
-			
-		}
-		private void SubscribeToEvents()
-		{
-			EventService.Instance.DiagButtonClickedByBot += OnDiagButtonClickedByBot;
-		}
-		private void UnsubscribeFromEvents()
-		{
-			EventService.Instance.DiagButtonClickedByBot -= OnDiagButtonClickedByBot;
-		}
+    public class ShopManager : MonoSingleton<ShopManager>
+    {
+        private void Start()
+        {
+            SubscribeToEvents();
+        }
 
-		public void OnDiagButtonClickedByBot(ButtonType buttonType)
-		{
-			if(buttonType == ButtonType.Shop)
-			{
-				var hero = HeroManager.Instance.GetCurrentHero();
-				var shopList = hero.ShopLists.Dequeue();
-				hero.ShopLists.Enqueue(shopList);
-				ShopListRenderer.Instance.Render(shopList);
-			}
-		}
-	}
+        protected override void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                UnsubscribeFromEvents();
+            }
+            base.OnDestroy();
+
+        }
+        private void SubscribeToEvents()
+        {
+            EventService.Instance.DiagBtnClickedByBot += OnDiagButtonClickedByBot;
+        }
+        private void UnsubscribeFromEvents()
+        {
+            EventService.Instance.DiagBtnClickedByBot -= OnDiagButtonClickedByBot;
+        }
+
+        public void OnDiagButtonClickedByBot(ButtonType buttonType)
+        {
+            if (buttonType == ButtonType.Shop)
+            {
+                var hero = HeroManager.Instance.GetCurrentHero();
+                var shopList = hero.ShopLists.Dequeue();
+                hero.ShopLists.Enqueue(shopList);
+                ShopListRenderer.Instance.Render(shopList);
+            }
+        }
+    }
 }
