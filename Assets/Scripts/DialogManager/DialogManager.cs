aae8d45f75dd5dbde866dbeabd63f6a0ce712d7c
vladzabaluev <vladzabaluev014@gmail.com>
2023-07-09 17:35:07 +0300

    Complete Quest giver

--- Assets/Scripts/DialogManager/DialogManager.cs
+++ Assets/Scripts/DialogManager/DialogManager.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+using DG.Tweening;
 using Random = UnityEngine.Random;
 
 public class DialogManager : MonoSingleton<DialogManager>
 {
     [SerializeField] private HelloPhrasesConfig helloPhrasesConfig;
     [SerializeField] private DialogConfig dialogConfig;
+
     // Btns
     //[SerializeField] private Button _skipBtn; // 0
     //[SerializeField] private Button _nextBtn; // 1
@@ -21,21 +22,29 @@ public class DialogManager : MonoSingleton<DialogManager>
     //[SerializeField] private Button _ansBBtn; // 7
     //[SerializeField] private Button _backBtn; // 8
     [SerializeField] private TextMeshProUGUI _dialogueText;
+
     [SerializeField] private Button[] _btns;
     [SerializeField] private RectTransform _envelope;
 
+    [SerializeField] private Sprite _closedEnvelope;
+    [SerializeField] private Sprite _openEnvelope;
+    private RectTransform _defaultPosition;
 
     private string[] helloPhrases;
     private float allDurationMessage;
+
     private IEnumerator _dispDiagCoroutine;
     private IEnumerator _displayNextButtonWithDelay;
 
-    void Start()
+    private void Start()
     {
         helloPhrases = helloPhrasesConfig.Data;
         allDurationMessage = dialogConfig.AllDurationMessage;
 
         EventService.Instance.DiagButtonClicked += HandleDiagBbtnClicked;
+        EventService.Instance.QuestAssigned += OnQuestGivenToHero;
+
+        _defaultPosition = _envelope;
     }
 
     protected override void OnDestroy()
@@ -43,9 +52,8 @@ public class DialogManager : MonoSingleton<DialogManager>
         EventService.Instance.DiagButtonClicked -= HandleDiagBbtnClicked;
     }
 
-    void Update()
+    private void Update()
     {
-
     }
 
     public void UpdateDialogueText(string text)
@@ -88,7 +96,34 @@ public class DialogManager : MonoSingleton<DialogManager>
             _displayNextButtonWithDelay = null;
         }
         _dialogueText.gameObject.SetActive(false);
+
         _envelope.gameObject.SetActive(true);
+
+        _envelope.TryGetComponent(out Image image);
+        image.sprite = _openEnvelope;
+
+        _envelope.DOMoveX(_envelope.position.x + 950, 1f).OnComplete(() =>
+        {
+            _envelope.gameObject.TryGetComponent(out QuestSlot slot);
+            slot.enabled = true;
+        });
+    }
+
+    public void OnQuestGivenToHero(Quest quest)
+    {
+        _envelope.TryGetComponent(out Image image);
+        image.sprite = _closedEnvelope;
+
+        image.DOFade(0, 1f);
+        _envelope.DOMoveY(_envelope.position.y + 380, 1f).OnComplete(() =>
+        {
+            _envelope.DOMove(new Vector3(_envelope.position.x - 950, _envelope.position.y - 380), 0);
+            image.DOFade(1, 0f);
+            EventService.Instance.HeroLeftFromScreen.Invoke();
+            DisplayBlank();
+            _envelope.gameObject.TryGetComponent(out QuestSlot slot);
+            slot.enabled = false;
+        });
     }
 
     public void DisplayMain()
@@ -147,39 +182,48 @@ public class DialogManager : MonoSingleton<DialogManager>
                 _displayNextButtonWithDelay = null;
                 DisplayMain();
                 break;
+
             case ButtonType.Next:
                 DisplayMain();
                 break;
+
             case ButtonType.Shop:
                 DisplayBlank();
                 //sorry for this
                 ShopListRenderer.Instance.OnRenderComplete += OnShopRenderComplete;
                 break;
+
             case ButtonType.Talk:
                 DisplayTalk();
                 break;
+
             case ButtonType.GetQuest:
                 DisplayDropEnvelope();
                 break;
+
             case ButtonType.Exit:
                 EventService.Instance.HeroLeaving?.Invoke();
                 break;
+
             case ButtonType.AnsA:
                 DisplayMain();
                 break;
+
             case ButtonType.AnsB:
                 DisplayMain();
                 break;
+
             case ButtonType.Back:
                 break;
         }
-        
     }
+
     private void OnShopRenderComplete()
     {
         ShopListRenderer.Instance.OnRenderComplete -= OnShopRenderComplete;
         DisplayShop();
     }
+
     private IEnumerator InvokeWithDelay(Action action, float sec)
     {
         yield return new WaitForSeconds(sec);
@@ -189,5 +233,4 @@ public class DialogManager : MonoSingleton<DialogManager>
 
 public class BehaviorPatterns
 {
-
 }
