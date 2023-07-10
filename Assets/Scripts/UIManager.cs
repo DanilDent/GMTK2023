3c6e5a825b8c078efc8b9a3679c7fbf2efb88298
Danil Dent <farctate11@gmail.com>
2023-07-10 14:53:42 +0400

    fix nickname anims

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -86,6 +86,7 @@ public class UIManager : MonoSingleton<UIManager>
         Debug.Log("?????????");
         _heroAvatarRect.DOMoveX(_defaultHeroAvatarPosition.x - _animationPercentForComing * Screen.width, 1f).From();
         _heroAvatarImg.DOFade(1, 1);
+        _heroNicknameText.DOFade(1, 1);
         _heroAvatarImg.sprite = hero.CurrentAvatarParts[0].Value;
     }
 
@@ -99,11 +100,13 @@ public class UIManager : MonoSingleton<UIManager>
         seq.Append(_heroAvatarRect.DOMoveX(_defaultHeroAvatarPosition.x + _animationPercentForLeaving * Screen.width, 1f));
 
         _heroAvatarImg.DOFade(0, 1);
+        _heroNicknameText.DOFade(0, 1);
         seq.Append(_heroAvatarRect.DOMoveX(_defaultHeroAvatarPosition.x, 1f)).OnComplete(() =>
         {
             diagManager.DisplayHello();
         });
         _heroAvatarImg.DOFade(1, 1);
+        _heroNicknameText.DOFade(1, 1);
     }
 
     private void HandleHeroLeftFromScreen()
@@ -111,6 +114,7 @@ public class UIManager : MonoSingleton<UIManager>
         Debug.Log("????????????");
         Sequence seq = DOTween.Sequence();
         _heroAvatarImg.DOFade(0, 1);
+        _heroNicknameText.DOFade(0, 1);
         seq.Append(_heroAvatarRect.DOMoveX(_defaultHeroAvatarPosition.x + _animationPercentForLeaving * Screen.width, 1f)).OnComplete(() =>
         {
             GameManager.Instance.CurrentHeroNickname = string.Empty;
